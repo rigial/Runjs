bcd76f0f7fac44e5b5af9a3be3e81d6f3713fcfa
Rigial <kishorekumar@rigial.com>
2026-05-05 00:10:30 +0530

    added accessible name for button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -51,6 +51,7 @@ function Navbar() {
 
         <div className="md:hidden">
           <button
+            aria-label={isOpen ? "Close menu" : "Open menu"}
             onClick={() => setIsOpen(!isOpen)}
             className="focus:outline-none"
           >
